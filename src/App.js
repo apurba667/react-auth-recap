b9dba7836adcf7b606b18e02f22b05345fc2d775
APURBA ROY <apurbaanikroy@gmail.com>
2021-03-15 23:55:49 +0600

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig';
+import { useEffect, useState } from 'react';
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 function App() {
+  const [user, setUser] = useState({})
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  const GitProvider = new firebase.auth.GithubAuthProvider();
+  const handelGoogleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorCode, errorMessage, email);
+      });
+
+  }
+  const handelFacebookSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // The signed-in user info.
+        var user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var accessToken = credential.accessToken;
+
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        // ...
+      });
+  }
+  const handelGithubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(GitProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = credential.accessToken;
+
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handelGoogleSignIn}>Sign in using google</button>
+      <br />
+      <br />
+      <button onClick={handelFacebookSignIn}>Sign in using Facebook</button>
+      <br />
+      <button onClick={handelGithubSignIn}>Sign in using Github</button>
+      <h3>Email:  {user.email}</h3>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1LwR5EpUMh5dfrjyi2xvWohzusMH7REQ",
+    authDomain: "auth-recap-react11.firebaseapp.com",
+    projectId: "auth-recap-react11",
+    storageBucket: "auth-recap-react11.appspot.com",
+    messagingSenderId: "568002349174",
+    appId: "1:568002349174:web:5c7c4a950dcb2e82ad3757"
+  };
+
+  export default firebaseConfig;
